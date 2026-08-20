88bbb2a1b60179d0d9acc618671dc330d6ed69e0
Joao Latczuk <145179118+joaolatczuk@users.noreply.github.com>
2026-08-20 15:54:21 -0300

    CORREÇÃO DO LOGO E AFINS
    CORREÇÃO DA LOGO E CORREÇÃO DOS MINUTOS PARA PISCADAS , ADIÇÃO DE UM DOCUMENTO DOS PASSOS FUTUROS

--- js/modules/blink.js
+++ js/modules/blink.js
@@ -2,8 +2,8 @@
 export const LIMITE_EAR_PADRAO = 0.21;
 export let limiteEarAtual = LIMITE_EAR_PADRAO;
 
-export const PISCADAS_MINIMAS_POR_MINUTO = 5;
-export const JANELA_TEMPO_MS = 20 * 1000; //
+export const PISCADAS_MINIMAS_POR_MINUTO = 6;
+export const JANELA_TEMPO_MS = 60 * 1000; //
 
 let contadorPiscadasJanela = 0;
 let olhoFechado = false;
