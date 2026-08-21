51ac4460a62beca89e8e5177e430882cae29c04d
Joao Latczuk <145179118+joaolatczuk@users.noreply.github.com>
2026-08-21 04:49:47 -0300

    v1.0.6
    Atualização da posição do facemesh em smartphone.

--- js/modules/camera.js
+++ js/modules/camera.js
@@ -16,13 +16,25 @@ export async function iniciarCamera(videoElement, canvasElement, statusElement) 
 
         return new Promise((resolve) => {
             videoElement.onloadedmetadata = async () => {
-                // Seta as propriedades nativas que o TensorFlow.js lê
-                videoElement.width = videoElement.videoWidth || 640;
-                videoElement.height = videoElement.videoHeight || 480;
+                const w = videoElement.videoWidth || 640;
+                const h = videoElement.videoHeight || 480;
+
+                videoElement.width = w;
+                videoElement.height = h;
 
                 if (canvasElement) {
-                    canvasElement.width = videoElement.width;
-                    canvasElement.height = videoElement.height;
+                    canvasElement.width = w;
+                    canvasElement.height = h;
+                }
+
+                // Ajusta proporção no container do vídeo se for celular (orientação retrato)
+                const videoContainer = videoElement.closest('.video-container');
+                if (videoContainer) {
+                    if (w < h) {
+                        videoContainer.style.aspectRatio = `${w} / ${h}`;
+                    } else {
+                        videoContainer.style.aspectRatio = "4 / 3";
+                    }
                 }
                 
                 await videoElement.play();
